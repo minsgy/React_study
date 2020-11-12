e52b1f2c9c03d8df5ba4851a06218233bc0bc4d3
minsgy <alstjrchl97@naver.com>
2020-11-12 14:20:00 +0900

    React 기본세팅

---
+++ README.md
@@ -0,0 +1,5 @@
+# ⚡️React and Node.js
+
+## ✅ 참고 사이트
+❗️ 인프런 강의 [react&node.js](https://inf.run/zNF9)  
+🦁 멋쟁이사자처럼 강의 
---
+++ begin-react/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Hello from './start';
+function App() {
+  return (
+    <div><Hello /></div>
+  );
+}
+
+export default App;
