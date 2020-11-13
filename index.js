fb65939eb6329d7d8aa6d9c9f53cf76b2a7e1e5e
minsgy <alstjrchl97@naver.com>
2020-11-14 00:44:59 +0900

    [nodejs] : schema 만들기

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://minseok:qwer1234@nodebox.ca282.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:false
+}).then(() => console.log('MongoDB connected...'))
+.catch(err => console.log(err))
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
